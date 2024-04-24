c344c2ef1b989c3d3dd87382ef7e1c36b42e0d19
jay-so <merry9504@naver.com>
2024-04-24 23:52:59 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/systemdesign/fixedwindowcounter/FixedwindowcounterApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.fixedwindowcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedwindowcounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixedwindowcounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/systemdesign/fixedwindowcounter/FixedwindowcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.fixedwindowcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedwindowcounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
